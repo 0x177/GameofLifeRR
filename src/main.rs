6d8cf9b49d40d0ed4f31ff0621c2520035efc618
0x5 <kmalalamry8@protonmail.com>
2023-05-24 19:14:48 +0300

    ITS DONE IT ONLY TOOK A COUPLE HOURS ME SO HAPPY

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 use rand::Rng;
 use raylib::prelude::*;
 
+//TODO: seperate to multiple src files
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let win_props: Vec<i32> = vec![200,200,10];     //w,h,res
-    let cols      = win_props[0] / win_props[2];
-    let rows      = win_props[1] / win_props[2];
-    let mut grid      = makeGrid(cols,rows);
+    let win_props: Vec<i32> = vec![640,480,10];     //w,h,res
+    let cols : i32 = win_props[0] / win_props[2];
+    let rows : i32 = win_props[1] / win_props[2];
+    let mut grid = make_grid(cols,rows);
 
     for i in 0..cols {
         for j in 0..rows {
@@ -32,6 +33,27 @@ fn main() {
             }
         }
 
+        let mut next = make_grid(cols,rows);
+
+        for i in 0..cols {
+            for j in 0..rows {
+            let state = grid[i as usize][j as usize];
+            // Count live neighbors!
+            let sum = 0;
+            let neighbors = countNeighbors(grid.clone(), i, j,cols,rows);
+
+            if state == 0 && neighbors == 3 {
+                next[i as usize][j as usize] = 1;
+            } else if state == 1 && (neighbors < 2 || neighbors > 3) {
+                next[i as usize][j as usize] = 0;
+            } else {
+                next[i as usize][j as usize] = state;
+            }
+    }
+  }
+
+        grid = next;
+
     }
 }
 
@@ -47,7 +69,21 @@ fn open_window(win_props: Vec<i32>, name: &str) -> (raylib::RaylibHandle, raylib
 }
 
 //TODO:migrate to matrices for a more largebra oriented design
-fn makeGrid(cols: i32,rows: i32)-> Vec<Vec<u64>> {
+fn make_grid(cols: i32,rows: i32)-> Vec<Vec<u64>> {
     let mut vec: Vec<Vec<u64>> = vec![vec![0; rows as usize];cols as usize];
     return vec;
 }
+
+fn countNeighbors(grid: Vec<Vec<u64>>, x: i32, y: i32,cols: i32, rows: i32) -> u64 {
+  let mut sum = 0;
+  for i in 0..3 {
+    for j in 0..3 {
+      let col = (x + i-1 + cols) % cols;
+      let row = (y + j-1 + rows) % rows;
+      sum = sum + grid[col as usize][row as usize];
+    }
+  }
+  sum = sum - grid[x as usize][y as usize];
+  return sum;
+}
+
